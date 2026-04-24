9c534d739fb266e07f5af36fde851ac3a0e1e846
cs250949 <cs250949@etu.univ-lehavre.fr>
2026-04-24 12:52:31 +0200

    Create Joueur.java

---
+++ src/Joueur.java
@@ -0,0 +1,20 @@
+/**
+ * C'est ici que vous pouvez modifier votre chemin, pour vous déplacer dans le labyrinthe.
+ */
+public class Joueur {
+
+    /**
+     * Méthode à implémenter pour déplacer le personnage.
+     * @param x Position X actuelle
+     * @param y Position Y actuelle
+     * @param grille Vision du labyrinthe
+     * @return "HAUT", "BAS", "GAUCHE" ou "DROITE"
+     */
+    public String deplacer(int x, int y, char[][] grille) {
+        // --- À VOUS DE JOUER ---
+        if (y < 5) {
+            return "DROITE";
+        } else {
+            return "BAS";
+        }
+    }
