fd6c7edbcd278d7e12db771e947c06b341de992f
anjoo <akingbadeanjola@gmail.com>
2026-04-28 14:29:25 +0200

    Update Labyrinthe.java

--- src/Labyrinthe.java
+++ src/Labyrinthe.java
@@ -1,258 +1,347 @@
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.util.ArrayList;
 
 public class Labyrinthe extends JFrame {
 
-    int x;
-    int y;
-    int niveau = 0;
+	int x;
+	int y;
+	int niveau = 0;
+	int score = 0;
+
+	ArrayList<Point> chats = new ArrayList<>();
+
+	char[][][] niveaux = {
+
+		{
+			{'#','#','#','#','#','#','#'},
+			{'#','P','.','.','M','S','#'},
+			{'#','.','#','.','.','.','#'},
+			{'#','.','.','.','C','.','#'},
+			{'#','#','#','#','#','#','#'}
+		},
+
+		{
+			{'#','#','#','#','#','#','#','#'},
+			{'#','P','.','.','.','#','M','#'},
+			{'#','.','#','#','.','#','.','#'},
+			{'#','.','.','#','.','.','.','#'},
+			{'#','#','.','#','#','#','.','#'},
+			{'#','.','.','.','.','#','.','#'},
+			{'#','C','.','#','.','.','S','#'},
+			{'#','#','#','#','#','#','#','#'}
+		},
+
+		{
+			{'#','#','#','#','#','#','#','#','#'},
+			{'#','P','.','#','.','.','.','M','#'},
+			{'#','.','#','#','.','#','#','.','#'},
+			{'#','.','.','.','.','#','.','.','#'},
+			{'#','#','#','#','.','#','.','#','#'},
+			{'#','.','.','.','.','.','.','#','#'},
+			{'#','.','#','#','#','#','.','.','#'},
+			{'#','C','.','.','.','.','.','S','#'},
+			{'#','#','#','#','#','#','#','#','#'}
+		},
+
+		{
+			{'#','#','#','#','#','#','#','#','#'},
+			{'#','P','.','.','M','.','.','M','#'},
+			{'#','.','#','#','#','#','.','#','#'},
+			{'#','.','.','.','.','#','.','.','#'},
+			{'#','#','#','#','.','#','#','.','#'},
+			{'#','.','.','.','.','.','.','.','#'},
+			{'#','.','#','#','#','#','#','.','#'},
+			{'#','C','.','.','.','.','.','S','#'},
+			{'#','#','#','#','#','#','#','#','#'}
+		},
+
+		{
+			{'#','#','#','#','#','#','#','#','#','#'},
+			{'#','P','.','.','.','#','.','.','M','#'},
+			{'#','.','#','#','.','#','.','#','.','#'},
+			{'#','.','.','#','.','.','.','#','.','#'},
+			{'#','#','.','#','#','#','.','#','.','#'},
+			{'#','.','.','.','.','#','.','.','.','#'},
+			{'#','.','#','#','.','#','#','#','.','#'},
+			{'#','C','.','#','.','.','.','#','C','#'},
+			{'#','.','.','.','.','.','.','.','S','#'},
+			{'#','#','#','#','#','#','#','#','#','#'}
+		}
+	};
+
+	char[][] grille;
+
+	JTextArea zoneCode;
+	JPanel panel;
+
+	public Labyrinthe() {
+
+		this.setTitle("Labyrinthe Chat & Souris");
+		this.setSize(600, 600);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setLayout(new BorderLayout());
+
+		this.zoneCode = new JTextArea();
+		this.add(this.zoneCode, BorderLayout.SOUTH);
+
+		JButton bouton = new JButton("Executer");
+		this.add(bouton, BorderLayout.NORTH);
+
+		this.panel = new JPanel() {
+			protected void paintComponent(Graphics g) {
+				super.paintComponent(g);
+				dessiner(g);
+			}
+		};
+
+		this.add(this.panel, BorderLayout.CENTER);
+
+		this.chargerNiveau();
+
+		bouton.addActionListener(new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				executer();
+			}
+		});
+
+		this.setupKeyBindings();
 
-    char[][][] niveaux = {
+		this.setVisible(true);
+	}
 
-        {{'#','#','#','#','#','#'}, {'#','P','.','.','S','#'}, {'#','#','#','#','#','#'}},
+	void setupKeyBindings() {
 
-        {{'#','#','#','#','#','#','#'}, {'#','P','.','.','.','S','#'}, {'#','#','#','#','#','#','#'}},
+		InputMap im = this.panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
+		ActionMap am = this.panel.getActionMap();
 
-        {{'#','#','#','#','#'}, {'#','P','.','#','#'}, {'#','#','.','S','#'}, {'#','#','#','#','#'}},
+		im.put(KeyStroke.getKeyStroke("UP"), "haut");
+		im.put(KeyStroke.getKeyStroke("DOWN"), "bas");
+		im.put(KeyStroke.getKeyStroke("LEFT"), "gauche");
+		im.put(KeyStroke.getKeyStroke("RIGHT"), "droite");
 
-        {{'#','#','#','#','#','#'}, {'#','P','.','.','#','#'}, {'#','#','#','.','.','#'}, {'#','#','#','#','S','#'}},
+		am.put("haut", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				jouer("HAUT");
+			}
+		});
+
+		am.put("bas", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				jouer("BAS");
+			}
+		});
+
+		am.put("gauche", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				jouer("GAUCHE");
+			}
+		});
+
+		am.put("droite", new AbstractAction() {
+			public void actionPerformed(ActionEvent e) {
+				jouer("DROITE");
+			}
+		});
+	}
 
-        {{'#','#','#','#','#','#','#'}, {'#','P','.','#','.','.','#'}, {'#','.','.','#','.','#','#'}, {'#','#','.','.','.','S','#'}},
+	void jouer(String direction) {
 
-        {{'#','#','#','#','#','#','#','#'}, {'#','P','.','.','#','.','.','#'}, {'#','#','#','.','#','.','#','#'}, {'#','.','.','.','.','.','S','#'}},
+		this.deplacer(direction);
 
-        {{'#','#','#','#','#','#','#','#'}, {'#','P','.','.','.','.','.','#'}, {'#','.','.','#','#','#','.','#'}, {'#','.','#','.','.','.','S','#'}},
+		if (Math.random() < 0.5) {
+			this.deplacerChats();
+		}
 
-        {{'#','#','#','#','#','#','#','#','#'}, {'#','P','.','.','.','#','.','.','#'}, {'#','#','#','#','.','#','.','#','#'}, {'#','.','.','.','.','.','.','S','#'}},
+		this.panel.repaint();
+
+		// collision avec un chat
+		for (Point chat : this.chats) {
+			if (this.x == chat.x && this.y == chat.y) {
+				JOptionPane.showMessageDialog(this, "Le chat t'a attrapé !");
+				this.niveau = 0;
+				this.score = 0;
+				this.chargerNiveau();
+				return;
+			}
+		}
 
-        {{'#','#','#','#','#','#','#','#','#','#'}, {'#','P','.','.','#','.','.','.','.','#'}, {'#','#','#','.','#','#','#','.','#','#'}, {'#','.','.','.','.','.','.','.','S','#'}, {'#','#','#','#','#','#','#','#','#','#'}}
-    };
+		// victoire
+		if (this.grille[this.x][this.y] == 'S') {
 
-    char[][] grille;
+			JOptionPane.showMessageDialog(this, "Niveau réussi !");
+			this.niveau++;
 
-    JTextArea zoneCode;
-    JPanel panel;
+			if (this.niveau == this.niveaux.length) {
+				JOptionPane.showMessageDialog(this, "Bravo ! Score = " + this.score);
+				return;
+			}
 
-    public Labyrinthe() {
+			this.chargerNiveau();
+		}
+	}
 
-        this.setTitle("Labyrinthe");
-        this.setSize(600, 600);
-        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        this.setLayout(new BorderLayout());
+	void chargerNiveau() {
+		this.grille = this.copier(this.niveaux[this.niveau]);
+		this.trouverPositions();
+	}
 
-        this.zoneCode = new JTextArea();
-        this.add(this.zoneCode, BorderLayout.SOUTH);
+	void dessiner(Graphics g) {
 
-        JButton bouton = new JButton("Executer");
-        this.add(bouton, BorderLayout.NORTH);
+		int largeur = this.panel.getWidth();
+		int hauteur = this.panel.getHeight();
 
-        this.panel = new JPanel() {
-            protected void paintComponent(Graphics g) {
-                super.paintComponent(g);
-                dessiner(g);
-            }
-        };
+		int nbColonnes = this.grille[0].length;
+		int nbLignes = this.grille.length;
 
-        this.add(this.panel, BorderLayout.CENTER);
+		int taille = Math.min(largeur / nbColonnes, hauteur / nbLignes);
 
-        this.chargerNiveau();
+		int offsetX = (largeur - taille * nbColonnes) / 2;
+		int offsetY = (hauteur - taille * nbLignes) / 2;
 
-        bouton.addActionListener(new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                executer();
-            }
-        });
+		for (int i = 0; i < this.grille.length; i++) {
+			for (int j = 0; j < this.grille[i].length; j++) {
 
-        this.setupKeyBindings();
+				if (this.grille[i][j] == '#') g.setColor(Color.PINK);
+				else if (this.grille[i][j] == 'S') g.setColor(Color.GREEN);
+				else if (this.grille[i][j] == 'M') g.setColor(Color.ORANGE);
+				else g.setColor(Color.WHITE);
 
-        this.setVisible(true);
-    }
+				g.fillRect(offsetX + j * taille, offsetY + i * taille, taille, taille);
+				g.setColor(Color.BLACK);
+				g.drawRect(offsetX + j * taille, offsetY + i * taille, taille, taille);
+			}
+		}
 
-    void setupKeyBindings() {
+		// joueur
+		g.setColor(Color.MAGENTA);
+		g.fillOval(offsetX + this.y * taille + taille/4,
+				   offsetY + this.x * taille + taille/4,
+				   taille/2, taille/2);
 
-        InputMap im = this.panel.getInputMap(JComponent.WHEN_IN_FOCUSED_WINDOW);
-        ActionMap am = this.panel.getActionMap();
+		// chats
+		g.setColor(Color.RED);
+		for (Point chat : this.chats) {
+			g.fillOval(offsetX + chat.y * taille + taille/4,
+					   offsetY + chat.x * taille + taille/4,
+					   taille/2, taille/2);
+		}
 
-        im.put(KeyStroke.getKeyStroke("UP"), "haut");
-        im.put(KeyStroke.getKeyStroke("DOWN"), "bas");
-        im.put(KeyStroke.getKeyStroke("LEFT"), "gauche");
-        im.put(KeyStroke.getKeyStroke("RIGHT"), "droite");
+		g.setColor(Color.BLACK);
+		g.drawString("Score : " + this.score, 10, 20);
+	}
 
-        am.put("haut", new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                jouer("HAUT");
-            }
-        });
+	void executer() {
 
-        am.put("bas", new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                jouer("BAS");
-            }
-        });
+		String[] lignes = this.zoneCode.getText().toUpperCase().split("\\n");
 
-        am.put("gauche", new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                jouer("GAUCHE");
-            }
-        });
+		Thread t = new Thread(new Runnable() {
+			public void run() {
 
-        am.put("droite", new AbstractAction() {
-            public void actionPerformed(ActionEvent e) {
-                jouer("DROITE");
-            }
-        });
-    }
+				for (int i = 0; i < lignes.length; i++) {
 
-    void jouer(String direction) {
+					String ligne = lignes[i].trim();
 
-        this.deplacer(direction);
-        this.panel.repaint();
+					deplacer(ligne);
+					deplacerChats();
+					panel.repaint();
 
-        if (this.grille[this.x][this.y] == 'S') {
+					try {
+						Thread.sleep(300);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		});
 
-            JOptionPane.showMessageDialog(this, "Niveau réussi !");
-            this.niveau = this.niveau + 1;
+		t.start();
+	}
 
-            if (this.niveau == this.niveaux.length) {
-                JOptionPane.showMessageDialog(this, "Bravo tu as fini !");
-                return;
-            }
+	void deplacer(String dir) {
 
-            this.chargerNiveau();
-            this.panel.repaint();
-        }
-    }
+		int nx = this.x;
+		int ny = this.y;
 
-    void chargerNiveau() {
-        this.grille = this.copier(this.niveaux[this.niveau]);
-        this.trouverDepart();
-    }
+		if (dir.equals("HAUT")) nx--;
+		if (dir.equals("BAS")) nx++;
+		if (dir.equals("GAUCHE")) ny--;
+		if (dir.equals("DROITE")) ny++;
 
-    void dessiner(Graphics g) {
+		if (nx >= 0 && nx < this.grille.length && ny >= 0 && ny < this.grille[0].length) {
 
-        int taille = 40;
+			if (this.grille[nx][ny] != '#') {
+				this.x = nx;
+				this.y = ny;
 
-        for (int i = 0; i < this.grille.length; i++) {
-            for (int j = 0; j < this.grille[i].length; j++) {
+				if (this.grille[this.x][this.y] == 'M') {
+					this.score++;
+					this.grille[this.x][this.y] = '.';
+				}
+			}
+		}
+	}
 
-                if (this.grille[i][j] == '#') {
-                    g.setColor(Color.PINK);
-                }
-                else if (this.grille[i][j] == 'S') {
-                    g.setColor(Color.GREEN);
-                }
-                else {
-                    g.setColor(Color.WHITE);
-                }
+	void deplacerChats() {
 
-                g.fillRect(j * taille, i * taille, taille, taille);
+		for (Point chat : this.chats) {
 
-                g.setColor(Color.BLACK);
-                g.drawRect(j * taille, i * taille, taille, taille);
-            }
-        }
+			int choix = (int)(Math.random() * 4);
 
-        g.setColor(Color.MAGENTA);
-        g.fillOval(this.y * taille + 10, this.x * taille + 10, 20, 20);
-    }
+			int dx = 0;
+			int dy = 0;
 
-    void executer() {
+			if (choix == 0) dx = -1;
+			if (choix == 1) dx = 1;
+			if (choix == 2) dy = -1;
+			if (choix == 3) dy = 1;
 
-        String texte = this.zoneCode.getText().toUpperCase();
-        String[] lignes = texte.split("\\n");
+			int nx = chat.x + dx;
+			int ny = chat.y + dy;
 
-        Thread t = new Thread(new Runnable() {
-            public void run() {
+			if (nx >= 0 && nx < this.grille.length && ny >= 0 && ny < this.grille[0].length) {
+				if (this.grille[nx][ny] != '#') {
+					chat.x = nx;
+					chat.y = ny;
+				}
+			}
+		}
+	}
 
-                for (int i = 0; i < lignes.length; i++) {
+	void trouverPositions() {
 
-                    String ligne = lignes[i].trim();
+		this.chats.clear();
 
-                    deplacer(ligne);
-                    panel.repaint();
+		for (int i = 0; i < this.grille.length; i++) {
+			for (int j = 0; j < this.grille[i].length; j++) {
 
-                    if (grille[x][y] == 'S') {
+				if (this.grille[i][j] == 'P') {
+					this.x = i;
+					this.y = j;
+				}
 
-                        JOptionPane.showMessageDialog(null, "Niveau réussi !");
-                        niveau++;
+				if (this.grille[i][j] == 'C') {
+					this.chats.add(new Point(i, j));
+				}
+			}
+		}
+	}
 
-                        if (niveau == niveaux.length) {
-                            JOptionPane.showMessageDialog(null, "Bravo tu as fini !");
-                            return;
-                        }
+	char[][] copier(char[][] src) {
 
-                        chargerNiveau();
-                        panel.repaint();
-                        return;
-                    }
+		char[][] dest = new char[src.length][src[0].length];
 
-                    try {
-                        Thread.sleep(300);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-        });
+		for (int i = 0; i < src.length; i++) {
+			for (int j = 0; j < src[i].length; j++) {
+				dest[i][j] = src[i][j];
+			}
+		}
 
-        t.start();
-    }
+		return dest;
+	}
 
-    void deplacer(String dir) {
-
-        int nx = this.x;
-        int ny = this.y;
-
-        if (dir.equals("HAUT")) {
-            nx = nx - 1;
-        }
-        if (dir.equals("BAS")) {
-            nx = nx + 1;
-        }
-        if (dir.equals("GAUCHE")) {
-            ny = ny - 1;
-        }
-        if (dir.equals("DROITE")) {
-            ny = ny + 1;
-        }
-
-        if (nx >= 0 && nx < this.grille.length && ny >= 0 && ny < this.grille[0].length) {
-
-            if (this.grille[nx][ny] != '#') {
-                this.x = nx;
-                this.y = ny;
-            }
-        }
-    }
-
-    void trouverDepart() {
-
-        for (int i = 0; i < this.grille.length; i++) {
-            for (int j = 0; j < this.grille[i].length; j++) {
-
-                if (this.grille[i][j] == 'P') {
-                    this.x = i;
-                    this.y = j;
-                }
-            }
-        }
-    }
-
-    char[][] copier(char[][] src) {
-
-        char[][] dest = new char[src.length][src[0].length];
-
-        for (int i = 0; i < src.length; i++) {
-            for (int j = 0; j < src[i].length; j++) {
-                dest[i][j] = src[i][j];
-            }
-        }
-
-        return dest;
-    }
-
-    public static void main(String[] args) {
-        new Labyrinthe();
-    }
+	public static void main(String[] args) {
+		new Labyrinthe();
+	}
 }
